d747905d6a79b3a011633e81b6ef0b77b19c8ccb
clrkshne <84219409+clrkshne@users.noreply.github.com>
2022-12-08 21:13:04 +0800

    Logout

--- NEUSTClinic/src/app/signin/signin.page.ts
+++ NEUSTClinic/src/app/signin/signin.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,34 +10,12 @@ export class SigninPage implements OnInit {
   handlerMessage = '';
   roleMessage = '';
 
-  constructor(private alertController: AlertController, private router:Router) { }
+  constructor (private router:Router) { }
 
   ngOnInit() {
   }
-  async logoutalert() {
-    const alert = await this.alertController.create({
-      header: 'Are you sure you want to logout?',
-      buttons: [
-        {
-          text: 'Logout',
-          role: 'logout',
-          handler: () => {
-            this.handlerMessage = 'Alert canceled';
-          },
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            this.handlerMessage = 'Alert confirmed';
-          },
-        },
-      ],
-    });
-
-    await alert.present();
-
-    const { role } = await alert.onDidDismiss();
-    this.roleMessage = `Dismissed with role: ${role}`;
+  async home() 
+  {
+   this.router.navigate(['home'])
   }
 }
